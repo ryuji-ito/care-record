836d9fe68985732b679e2bf18100928469a1615a
ryuji-ito <eposodeskip@yahoo.co.jp>
2020-01-25 19:38:27 +0900

    vaiuos commit

--- app/controllers/floors_controller.rb
+++ app/controllers/floors_controller.rb
@@ -2,10 +2,25 @@ class FloorsController < ApplicationController
 
   def new
     @floor = Floor.new
-    @floor.users << current_user
+  end
+ 
+  def create
+    @floor = Floor.new(floor_params)
+    if @floor.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
-  def create
+  def edit
   end
-  
+
+  def update
+  end
+
+  private
+  def floor_params
+    params.require(:floor).permit(:name)
+  end
 end
